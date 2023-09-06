b3ef15aa2faf4efe653fabcce6f662e8ff26b8ad
Alex-Rubsam <142846602+Alex-Rubsam@users.noreply.github.com>
2023-09-06 11:10:18 -0600

    Update teamInfo.js

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -18,7 +18,10 @@ export const memberData = [
     },
     {
         name: "Team Member 2 ",
-        bio: "Passionate learner pursuing my computer science bachelor's degree at Colorado State University. Experienced worker with a background in construction and manufacturing, I have done everything from house painting to leading a department in a furniture manufacturing company. I am familiar with the demands of a working environment and aim to enhance my technical skills and apply them to a field that will being increased quality of life and accessibility to those who need it. Computers have always been a passion of mine and I love learning more about how they work and discovering amazing new ways to use them to solve complex problems and better the world.",
+        bio: "Passionate learner pursuing my computer science bachelor's degree at Colorado State University. Experienced worker with a background"
+        + " in construction and manufacturing, I have done everything from house painting to leading a department in a furniture manufacturing company."
+        + " I am familiar with the demands of a working environment and aim to enhance my technical skills and apply them to a field that will being increased quality of life and accessibility"
+        + " to those who need it. Computers have always been a passion of mine and I love learning more about how they work and discovering amazing new ways to use them to solve complex problems and better the world.",
         homeTown: "Hometown",
         imagePath: memberPic
     },
