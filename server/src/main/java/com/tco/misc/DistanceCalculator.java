ff813833fa72f8a657d83fce97b4b22a8d581913
David Riva <davidjriva@gmail.com>
2023-09-17 17:43:02 -0600

    Creating distance calc class

---
+++ server/src/main/java/com/tco/misc/DistanceCalculator.java
@@ -0,0 +1,10 @@
+package com.tco.misc;
+
+public final class DistanceCalculator {
+    //Stop users from instantiating using a default ctor
+    private DistanceCalculator(){ }
+
+    public static Long calculate(GeographicCoordinate from, GeographicCoordinate to, double earthRadius) {
+        return -1L;
+    }
+}
