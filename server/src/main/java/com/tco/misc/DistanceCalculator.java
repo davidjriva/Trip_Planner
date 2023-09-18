545aa9c5c0f5ece9d2f0d0979c726a6f46854ab9
David Riva <davidjriva@gmail.com>
2023-09-17 19:39:02 -0600

    logic errors in calculator

--- server/src/main/java/com/tco/misc/DistanceCalculator.java
+++ server/src/main/java/com/tco/misc/DistanceCalculator.java
@@ -14,18 +14,17 @@ public final class DistanceCalculator {
         Double phi1 = from.latRadians();
         Double phi2 = to.latRadians();
         Double lambda1 = from.lonRadians();
-        Double lambda2 = to.latRadians();
+        Double lambda2 = to.lonRadians();
         Double deltaLambda = lambda2 - lambda1;
 
         Double numerator = Math.pow(Math.cos(phi2)*Math.sin(deltaLambda), 2) + Math.pow(Math.cos(phi1)*Math.sin(phi2) - Math.sin(phi1)*Math.cos(phi2)*Math.cos(deltaLambda), 2);
-        numerator = Math.pow(numerator, 0.5);
+        numerator = Math.sqrt(numerator);
         
         Double denominator = Math.sin(phi1)*Math.sin(phi2) + Math.cos(phi1)*Math.cos(phi2)*Math.cos(deltaLambda);
 
         Double deltaSigma = Math.atan2(numerator, denominator);
 
         Double distance = earthRadius * deltaSigma;
-
         return Math.round(distance);
     }
 }
