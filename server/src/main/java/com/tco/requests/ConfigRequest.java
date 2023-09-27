51d460bb385e6b9f5335dbb75e40fd6ff97f0fb8
bachrock <4308229+bachrock@users.noreply.github.com>
2023-09-26 16:33:43 -0800

    Update ConfigRequest.java

--- server/src/main/java/com/tco/requests/ConfigRequest.java
+++ server/src/main/java/com/tco/requests/ConfigRequest.java
@@ -18,6 +18,7 @@ public class ConfigRequest extends Request {
         serverName = "t11 The Chronicles of JavaScript";
         features = new ArrayList<>();
         features.add("config");
+        features.add("distances");
         log.trace("buildResponse -> {}", this);
     }
 
