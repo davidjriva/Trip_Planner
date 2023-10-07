c9c54e0798b1f852aa1d31d184abd1659cf62910
David Riva <davidjriva@gmail.com>
2023-10-06 18:51:30 -0600

    Adds database operations class

---
+++ server/src/main/java/com/tco/database/DatabaseOperations.java
@@ -0,0 +1,30 @@
+package com.tco.database;
+
+import java.sql.ResultSet;
+import com.tco.database.sqlGuide.Place;
+import com.tco.database.sqlGuide.Places;
+
+public class DatabaseOperations {
+
+    public static Integer count(ResultSet results) throws Exception {
+        if (results.next()) {
+            return results.getInt("count");
+        }
+        throw new Exception("No count results in the found query.");
+    }
+
+    public static Places convertQueryResultsToPlaces(ResultSet results, String columns) throws Exception {
+        int count = 0;
+        String[] cols = columns.split(",");
+        Places places = new Places();
+        while (results.next()) {
+            Place place = new Place();
+            for (String col : cols) {
+                place.put(col, results.getString(col));
+            }
+            place.put("index", String.format("%d", ++count));
+            places.add(place);
+        }
+        return places;
+    }
+}
