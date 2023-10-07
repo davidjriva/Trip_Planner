d98da806c06ccc17a4ac1307e9f65335b6faea81
David Riva <davidjriva@gmail.com>
2023-10-07 10:22:34 -0600

    Adding tests for the sqlGuide

---
+++ server/src/test/java/com/tco/database/TestSqlGuide.java
@@ -0,0 +1,41 @@
+package com.tco.database;
+
+import com.tco.database.sqlGuide.Database;
+import com.tco.database.sqlGuide.Places;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
+
+public class TestSqlGuide {
+    @Test
+    @DisplayName("driva: Check DB Search")
+    public void testSearch() throws Exception {
+        int value;
+        Places places;
+        try{
+            value = Database.found("dave");
+            places = Database.places("dave", 3);
+
+            assertNotNull(places);
+            assertFalse(places.isEmpty());
+        } catch(Exception e){
+            throw new Exception("Error with: " + e.getMessage());
+        }
+        
+        assertEquals(places.get(0).get("continent"), "NA");
+        assertEquals(places.get(0).get("altitude"), "5170");
+        assertEquals(places.get(0).get("iso_country"), "US");
+        assertEquals(places.get(0).get("latitude"), "40.0332984924");
+        assertEquals(places.get(0).get("name"), "Dave's Airport");
+        assertEquals(places.get(0).get("municipality"), "Louisville");
+        assertEquals(places.get(0).get("iso_region"), "US-CO");
+        assertEquals(places.get(0).get("index"), "1");
+        assertEquals(places.get(0).get("id"), "0CO1");
+        assertEquals(places.get(0).get("longitude"), "-105.124000549");
+
+        assertEquals(places.size(), 3);
+        assertEquals(value, 16);
+    }
+}
