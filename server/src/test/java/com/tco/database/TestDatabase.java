9cfb043e90663581a70d0b0d714f2cad4ac4c122
Alex Rubsam <alex.rubsam@colostate.edu>
2023-10-19 06:15:26 +0000

    add tests

---
+++ server/src/test/java/com/tco/database/TestDatabase.java
@@ -0,0 +1,43 @@
+package com.tco.database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import com.tco.database.DatabaseConnector;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDatabase {
+
+    private static final transient Logger log = LoggerFactory.getLogger(TestDatabase.class);
+
+    @Test
+    @DisplayName("alexr11: test DatabaseConnector")
+    public void testConenctor() {
+        Connection conn;
+        ResultSet results;
+        try {
+        //c
+        conn = DatabaseConnector.connect();
+        assertEquals(conn.isValid(500), true);
+        results = DatabaseConnector.executeQuery(conn, "select * from world limit 5;");
+        assertEquals(results.getInt("found"), 2);
+        DatabaseConnector.closeConnection(conn);
+        assertEquals(conn.isValid(500), false);
+        } catch(Exception e){
+            log.error("Error with: " + e.getMessage());
+        
+        }
+
+        /*try {
+        //DatabaseConnector.closeConnection(conn);
+        } catch(Exception e){
+            log.error("Error with: " + e.getMessage());
+        } */
+    }
+}
