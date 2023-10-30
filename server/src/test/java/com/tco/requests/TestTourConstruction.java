902fdcf447505a86a954d29135a61cdd27a5debb
David Riva <davidjriva@gmail.com>
2023-10-29 18:23:32 -0600

    nn and tests

--- server/src/test/java/com/tco/requests/TestTourConstruction.java
+++ server/src/test/java/com/tco/requests/TestTourConstruction.java
@@ -43,4 +43,52 @@ public class TestTourConstruction{
         assertEquals(314L, distanceMatrix[0][1].longValue());
         assertEquals(629L, distanceMatrix[0][2].longValue());
     }
+
+    @Test
+    @DisplayName("driva: test nn single place")
+    public void testNearestNeighborSinglePlace() {
+        Place[] places = {
+            new Place("1.0", "2.0")
+        };
+
+        TourConstruction tc = new TourConstruction();
+        tc.nearestNeighbor(places);
+        int[] tourResults = tc.tourResults;
+        
+        assertNotNull(tourResults);
+        assertEquals(places.length, tourResults.length);
+        assertEquals(0, tourResults[0]);
+    }
+
+    @Test
+    @DisplayName("driva: test nn multiple places")
+    public void testNearestNeighborMultiplePlaces() {
+        Place[] places = {
+            new Place("1.0", "2.0"),
+            new Place("3.0", "4.0"),
+            new Place("5.0", "6.0")
+        };
+
+        TourConstruction tc = new TourConstruction();
+        tc.nearestNeighbor(places);
+        int[] tourResults = tc.tourResults;
+
+        assertNotNull(tourResults);
+        assertEquals(places.length, tourResults.length);
+
+        boolean[] visited = new boolean[places.length];
+        for (int city : tourResults) {
+            assertFalse(visited[city]);
+            visited[city] = true;
+        }
+
+        long totalDistance = 0;
+        for (int i = 0; i < tourResults.length; i++) {
+            int currCity = tourResults[i];
+            int nextCity = tourResults[(i + 1) % tourResults.length];
+            totalDistance += tc.distanceMatrix[currCity][nextCity];
+        }
+
+        assertEquals(1257L, totalDistance);
+    }
 }
