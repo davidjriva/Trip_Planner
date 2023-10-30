e1e72029cc024449d6abe794456c46a3780c3f7b
David Riva <davidjriva@gmail.com>
2023-10-30 15:34:39 -0600

    tourRequest class

---
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -0,0 +1,43 @@
+package com.tco.requests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.tco.database.sqlGuide.Database;
+import com.tco.database.sqlGuide.Place;
+import com.tco.database.sqlGuide.Places;
+
+public class TourRequest extends Request {
+    private static final transient Logger log = LoggerFactory.getLogger(FindRequest.class);
+
+    private Double earthRadius;
+    private Double response;
+    private Places places;
+
+    @Override
+    public void buildResponse() {
+        log.trace("buildResponse -> {}", this);
+    }
+
+
+    /* The following methods exist only for testing purposes and are not used
+    during normal execution, including the constructor. */
+    public TourRequest(Double earthRadius, Double response, Places places){
+        super();
+        this.requestType = "tour";
+        this.earthRadius = earthRadius;
+        this.response = response;
+    }
+
+    public Double earthRadius() {
+        return this.earthRadius;
+    }
+
+    public Double response() {
+        return this.response;
+    }
+
+    public Places places() {
+        return this.places;
+    }
+}
