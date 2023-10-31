104caeb558adfce169cbcfcc9fab6c755e17cadf
bachrock <bachrock@gmail.com>
2023-10-30 18:08:57 -0600

    addtests

--- server/src/test/java/com/tco/requests/TestTourConstruction.java
+++ server/src/test/java/com/tco/requests/TestTourConstruction.java
@@ -91,4 +91,50 @@ public class TestTourConstruction{
 
         assertEquals(1257L, totalDistance);
     }
+
+    @Test
+    @DisplayName ("bachrock: tests initializeRemainingCities with array of one value")
+    public void testInitializeRemainingCities() {
+        boolean[] remainingCities = new boolean[1];
+        initializeRemainingCities(remainingCities);
+
+        assertTrue(remainingCities[0]);
+    }
+
+    @Test
+    @DisplayName ("bachrock: tests initializeRemainingCities with false array of ten values")
+    public void testInitializeRemainingCities() {
+        boolean[] remainingCities = new boolean[10];
+        for(boolean remainingCity : remainingCities) {
+            remainingCity = false;
+        }
+        initializeRemainingCities(remainingCities);
+        for(boolean remainingCity : remainingCities) {
+            assertTrue(remainingCity);
+        }
+    }
+
+    @Test
+    @DisplayName ("bachrock: tests initializeRemainingCities with false array")
+    public void testInitializeRemainingCities() {
+        boolean[] remainingCities = new boolean[3];
+        remainingCities[0] = false;
+        remainingCities[1] = false;
+        remainingCities[2] = false;
+
+        initializeRemainingCities(remainingCities);
+
+        assertTrue(remainingCities[0]);
+        assertTrue(remainingCities[1]);
+        assertTrue(remainingCities[2]);
+    }
+
+    @Test
+    @DisplayName ("bachrock: tests initializeRemainingCities with large array")
+    public void testInitializeRemainingCities() {
+        boolean[] remainingCities = new boolean[1000000];
+        for(boolean remainingCity : remainingCities) {
+            assertTrue(initializeRemainingCities(remainingCity));
+        }
+    }
 }
