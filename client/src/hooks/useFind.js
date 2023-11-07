1c1f6f9c0269a9b0b4fa26d41141ee6a4af33a92
Alex Rubsam <alex.rubsam@colostate.edu>
2023-11-07 18:31:53 +0000

    add logic for random

--- client/src/hooks/useFind.js
+++ client/src/hooks/useFind.js
@@ -19,14 +19,23 @@ export function useFind(match) {
 
 async function makeFindRequest(match, findActions) {
     if (match.length >= 3){
-        const { setResults } = findActions;
-
         const defaultLimit = 10; 
         const requestBody = { requestType: 'find', match: match, limit: defaultLimit };
-        
-        //We have feature implemented:
+        makeRequest(findActions, requestBody);
+    }
+
+    else if (match === 1 || match === 0)
+    {
+        const defaultLimit = 1; 
+        const requestBody = { requestType: 'find', match: "", limit: defaultLimit };
+        makeRequest(findActions, requestBody);
+    }
+}
+
+async function makeRequest(findActions, requestBody)
+{
+        const { setResults } = findActions;
         const serverUrl = getOriginalServerUrl();
         const findResponse = await sendAPIRequest(requestBody, serverUrl);
         setResults(findResponse.places);
-    }
 }
