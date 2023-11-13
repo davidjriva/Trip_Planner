8033866024b4d146e527fb0bbc8bf904354330cd
David Riva <davidjriva@gmail.com>
2023-11-13 13:54:34 -0700

    endpoints

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -4,23 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TourRequest extends Request {
-    private static final transient Logger log = LoggerFactory.getLogger(FindRequest.class);
+    private static final transient Logger log = LoggerFactory.getLogger(TourRequest.class);
 
-    private Double earthRadius;
-    private Double response;
-    private Places places;
+    private double earthRadius;
+    private double response;
+    private Places places = new Places();
 
     @Override
     public void buildResponse() {
-        Opt1 optimize = new Opt1(places);
-        optimize.improve();
-        this.places = optimize.places;
+        // Opt1 optimize = new Opt1(places);
+        // optimize.improve();
+        // this.places = optimize.places;
         log.trace("buildResponse -> {}", this);
     }
 
-
     /* The following methods exist only for testing purposes and are not used
     during normal execution, including the constructor. */
+
     public TourRequest(Double earthRadius, Double response, Places places){
         super();
         this.requestType = "tour";
