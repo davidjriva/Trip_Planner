e97c9d7f47cca284a0d92cd6b49dbe20b18cc118
David Riva <davidjriva@gmail.com>
2023-11-17 11:41:27 -0700

    2opt reverse

--- server/src/main/java/com/tco/requests/Opt2.java
+++ server/src/main/java/com/tco/requests/Opt2.java
@@ -10,6 +10,16 @@ public class Opt2 extends Tour {
 
     }
 
+    public void twoOptReverse(int[] route, int i1, int k) {
+        while (i1 < k) {
+            int temp = route[i1];
+            route[i1] = route[k];
+            route[k] = temp;
+            i1++;
+            k--;
+        }
+    }
+
     public int legDis(int[] route, int i, int j) {
         int distance = 0;
         for (int index = i; index < j && index < route.length - 1; index++) {
