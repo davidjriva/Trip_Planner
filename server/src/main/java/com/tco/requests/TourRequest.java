72ccac77ff9dd98f8108a9c3e4ceecaa42af3205
David Riva <davidjriva@gmail.com>
2023-12-04 13:18:14 -0700

    fixes equation

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -2,6 +2,7 @@ package com.tco.requests;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.lang.Math;
 
 public class TourRequest extends Request {
     private static final transient Logger log = LoggerFactory.getLogger(TourRequest.class);
@@ -11,7 +12,7 @@ public class TourRequest extends Request {
     private Places places = new Places();
 
     public boolean shouldPerform1Opt(int numPlaces) {
-        return response <= ((0.0001 * numPlaces) + (0.45 * numPlaces) + 100);
+        return response <= ((0.0001 * Math.pow(numPlaces, 2)) + (0.45 * numPlaces) + 100);
     }
 
     @Override
